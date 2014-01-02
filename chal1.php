9635ea9990cdc74259ccfd137a1550282451f4b4
Derek Remund <ddremund@gmail.com>
2014-01-02 14:27:48 -0600

    php-opencloud installed

---
+++ chal1.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'C:/Users/ddremund/Downloads/vendor/autoload.php';
+
+use OpenCloud\Rackspace;
+
+if ($_SERVER['OS'] == 'Windows_NT')
+	$homedir = $_SERVER['HOMEDRIVE'] . $_SERVER['HOMEPATH'] . '\\';
+else
+	$homedir = $_SERVER['HOME'] . '/';
+
+print "Using creds file in $homedir...\n";
+
+$creds = parse_ini_file($homedir . '.rackspace_cloud_credentials');
+
+$client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
+	'username' => $creds['username'],
+	'apiKey' => $creds['api_key']
+));
+
+$compute = $client->computeService('cloudServersOpenStack', 'IAD');
+
+
+
+?>
